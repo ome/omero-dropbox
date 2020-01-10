93fb34f7c939c0289ed303f2770e0e5231a733f4
jmoore <josh@glencoesoftware.com>
2020-01-10 14:23:58 +0100

    Bump to 5.6.0

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
-   Copyright 2008-2019 The Open Microscopy Environment, Glencoe Software, Inc.
+   Copyright 2008-2020 The Open Microscopy Environment, Glencoe Software, Inc.
    All rights reserved.
 
    Use is subject to license terms supplied in LICENSE.txt
@@ -14,7 +14,7 @@ import os
 
 from setuptools import setup
 
-VERSION = "5.6.dev4"
+VERSION = "5.6.0"
 
 url = 'https://docs.openmicroscopy.org/latest/omero/developers/Server/FS.html'
 
