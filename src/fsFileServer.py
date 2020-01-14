189866f402e63aa1173cf8994d1d322dc499ecdd
jmoore <josh@glencoesoftware.com>
2020-01-14 13:43:48 +0100

    Fix further import errors

--- src/fsFileServer.py
+++ src/fsFileServer.py
@@ -19,10 +19,10 @@ except:
 # functionality of os.path but without the complexity.
 # Imported as pathModule to avoid potential clashes.
 try:
-    from omero_ext.path import path as pathModule
+    from omero_ext import path as pathModule
 except ImportError:
     # Python 2
-    from path import path as pathModule
+    import path as pathModule
 
 import omero.all
 import omero.grid.monitors as monitors
